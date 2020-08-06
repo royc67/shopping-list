7d7ab40f508a4b914ef7b84442719a2fbce079b2
royc67 <royc67@gmail.com>
2020-08-07 00:14:48 +0300

    #1

---
+++ Front-End/main.js
@@ -0,0 +1,5 @@
+const displayDiv = document.getElementById('displayDiv')
+const inputProduct = document.getElementById('inputProduct')
+const addButton = document.getElementById('addButton')
+
+//const shoppingList = 
