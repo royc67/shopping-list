0f1752843ed87e24f136c06324060c33358738e9
royc67 <royc67@gmail.com>
2020-08-07 15:37:00 +0300

    Update front.js

--- Front-End/front.js
+++ Front-End/front.js
@@ -4,9 +4,7 @@ const addButton = document.getElementById('addButton')
 
 async function getProducts() {
     const {data} = await axios.get(`http://localhost:8080/products`)
-    debugger;
         data.forEach((product) => {
-            debugger;
             console.log(product)
             makeRow(product.id, product.name)
         });
@@ -17,6 +15,11 @@ addButton.addEventListener('click', ()=>{getProducts()})
 const makeRow = (id,name) => {
     const newRow = document.createElement('li')
     const deleteButton = document.createElement('button')
+    deleteButton.innerHTML = "X";
+    deleteButton.addEventListener('click', ()=>{
+        deleteProduct(id);
+        e.target.parentElement.remove();
+    })
     const idInput = document.createElement('input')
     const nameInput = document.createElement('input')
     idInput.disabled = true
@@ -25,25 +28,24 @@ const makeRow = (id,name) => {
     nameInput.disabled = true
     nameInput.value = name
     nameInput.className = id
-    deleteButton.innerHTML = "X";
+    
     const editButton = document.createElement('button')
+    const doneButton = document.createElement('button')
+    doneButton.style.display = "none";
     editButton.innerHTML = "Edit"
-    editButton.addEventListener('click', ()=>{
-        const inputs = document.getElementsByClassName(id)
-        inputs[0].disabled = false;
-        inputs[1].disabled = false;
-    })
     const inputs = newRow.getElementsByTagName('input')
     inputs.disabled = false;
     newRow.appendChild(idInput)
     newRow.appendChild(nameInput)
+    newRow.appendChild(doneButton)
     newRow.appendChild(editButton)
     newRow.appendChild(deleteButton)
     display.appendChild(newRow)
 }
 
-async function deleteProducts(id) {
-    await axios.delete(`http://localhost:8080/products${id}`)
+
+async function deleteProduct(id) {
+    await axios.delete(`http://localhost:8080/products/${id}`)
 }
 
 async function updateProduct(id,name) {
